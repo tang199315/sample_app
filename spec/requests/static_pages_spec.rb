0cfd629d50d9c2eac59785256b0f569c32d5bdc5
leon.chan <tang199315@gmail.com>
2014-09-04 11:11:20 +0800

    refactor rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,69 +1,32 @@
 require 'spec_helper'
-	describe "Static pages" do
+
+describe "Static pages" do
+	subject{ page }
 
 	describe "Home page" do
-		let(:title) { "Ruby on Rails Tutorial Sample App " }
-
-		it "should have the content 'Sample App'" do
-		visit root_path
-		page.should have_selector('h1',
-									:text => 'Sample App')
-		end
-
-		it "should have the title 'Home'" do
-		visit root_path
-		page.should have_selector('title',
-									:text => title + '| Home')
-		end
+		before(:each){ visit root_path}
+		it { should have_selector('h1', :text => 'Sample App') }
+		it { should have_selector('title', :text => full_title('')) }
+		it { should_not have_selector('title', :text => '| Home') }
 
 	end
 
 	describe "Help page" do
-		let(:title) { "Ruby on Rails Tutorial Sample App " }
-
-		it "should have the content 'Help'" do
-		visit help_path
-		page.should have_selector('h1',
-									:text => 'Help')
-		end
-
-		it "should have the title 'Help'" do
-		visit help_path
-		page.should have_selector('title',
-									:text => title + '| Help')
-		end
-
+		before(:each){ visit help_path}
+		it { should have_selector('h1', :text => 'Help') }
+		it { should have_selector('title', :text => full_title('Help'))  }
 	end
 
 	describe "About Page" do
-		let(:title) { "Ruby on Rails Tutorial Sample App " }
-		it "should have the content 'About us'" do
-		visit about_path
-		page.should have_selector('h1',
-									:text => 'About Us')
-		end
-		
-		it "should have the title 'About Us'" do
-		visit about_path
-		page.should have_selector('title',
-									:text => title + '| About Us')
-		end
-
+		before(:each){ visit about_path}
+		it { should have_selector('h1', :text => 'About Us') }
+		it { should have_selector('title', :text => full_title('About Us'))  }
 	end
 
 	describe "Contact Page" do
-		let(:title) { "Ruby on Rails Tutorial Sample App " }
-		it "should have the content 'Contact'" do
-		visit contact_path
-		page.should have_selector('h1',
-									:text => 'Contact')
-		end
-		
-		it "should have the title 'Contact'" do
-		visit contact_path
-		page.should have_selector('title',
-									:text => title + '| Contact')
-		end
+		before(:each){ visit contact_path}
+		it { should have_selector('h1', :text => 'Contact') }
+		it { should have_selector('title', :text => full_title('Contact'))  }
 
 	end
 
---
+++ spec/support/utilities.rb
@@ -0,0 +1,8 @@
+def full_title(subtitle)
+	base_title = 'Ruby on Rails Tutorial Sample App'
+	if subtitle.empty?
+		base_title
+	else
+		"#{base_title} | #{subtitle}"
+	end
+end
