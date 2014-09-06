c509057ccc25423bdca7cd44aaf3ccbf56aab3d4
leon.chan <tang199315@gmail.com>
2014-09-06 18:33:38 +0800

    Finish Chapter 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,6 +62,15 @@ describe User do
 		end
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+		it "should save as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
 	describe "when email address is already token" do
 		before do
 			user_with_same_email = @user.dup
